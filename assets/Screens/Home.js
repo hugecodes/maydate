ae366be660057309172035d9adf4825974d26b99
drewminns <drew.minns@me.com>
2018-02-09 13:51:30 -0500

    Added audio permissions

--- assets/Screens/Home.js
+++ assets/Screens/Home.js
@@ -1,14 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { Permissions } from 'expo';
 
 export default class Home extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      haveRecordingPermissions: false
+    }
+  }
+
+  componentDidMount() {
+    this.getAccessToRecording();
+  }
+
   render() {
+    const { haveRecordingPermissions } = this.state;
+
     return (
       <View>
         <Text>Home Screen</Text>
+        { haveRecordingPermissions && <Text>We Good</Text> }
       </View>
     )
+  }
+
+  getAccessToRecording = async () => {
+    console.log('running');
+    try {
+      const { status } = await Permissions.askAsync(Permissions.CONTACTS);
+      if (status === 'granted') {
+        this.setState({ haveRecordingPermissions: true });
+      }
+    } catch(error) {
+      console.log(error);
+    }
+    
   }
 
 }
