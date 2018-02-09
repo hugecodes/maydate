9263b1c4d5363574993f7a8c0f6410f6fd9138d4
Cory LaFlamme <claflamme@hugeinc.com>
2018-02-09 10:39:37 -0500

    Added phone number field and non-functioning button.

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      phoneNumber: '',
+    };
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={ styles.container }>
+        <Text>Phone number to call:</Text>
+        <TextInput
+          style={ styles.input }
+          keyboardType='phone-pad'
+          placeholder='Put yer number here'
+          onChangeText={ this.setPhoneNumber }
+        />
+        <Button
+          title='Save and arm'
+          style={{ fontSize: 28, padding: 10 }}
+        />
       </View>
     );
+  }
+
+  setPhoneNumber = (phoneNumber) => {
+    this.setState({ phoneNumber });
   }
 }
 
@@ -16,6 +38,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    marginTop: 150,
   },
+  input: {
+    height: 40,
+    width: '80%',
+    fontSize: 24,
+    marginTop: 30,
+    marginBottom: 30,
+    padding: 5,
+  }
 });
